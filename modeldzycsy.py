e3cce54704bda14831cc18525398da7db7b28d07
dipth11 <3120297482@qq.com>
2023-01-15 14:11:04 +0800

    Initial commit

--- modeldzycsy.py
+++ modeldzycsy.py
@@ -163,25 +163,26 @@ class FCUDown(nn.Module):
     """ CNN feature maps -> Transformer patch embeddings
     """
 
-    def __init__(self, inplanes, outplanes, dw_stride, act_layer=nn.GELU,
+    def __init__(self, inplanes, outplanes, dw_stride, proj, act_layer=nn.GELU,
                  norm_layer=partial(nn.LayerNorm, eps=1e-6)):
         super(FCUDown, self).__init__()
         self.dw_stride = dw_stride
 
         self.conv_project = nn.Conv2d(inplanes, outplanes, kernel_size=1, stride=1, padding=0)
         self.sample_pooling = nn.AvgPool2d(kernel_size=dw_stride, stride=dw_stride)
+        self.linear_proj = proj
 
         self.ln = norm_layer(outplanes)
         self.act = act_layer()
 
     def forward(self, x, x_t):
         x = self.conv_project(x)  # [N, C, H, W]
-
-        x = self.sample_pooling(x).flatten(2).transpose(1, 2)
+        x = self.sample_pooling(x).flatten(2) # N C HxW
+        x = self.linear_proj(x).transpose(1, 2)
         x = self.ln(x)
         x = self.act(x)
 
-        x = torch.cat([x_t[:, 0][:, None, :], x], dim=1)
+        # x = torch.cat([x_t[:, 0][:, None, :], x], dim=1)
 
         return x
 
@@ -190,22 +191,24 @@ class FCUUp(nn.Module):
     """ Transformer patch embeddings -> CNN feature maps
     """
 
-    def __init__(self, inplanes, outplanes, up_stride, act_layer=nn.ReLU,
+    def __init__(self, inplanes, outplanes, up_stride, proj, act_layer=nn.ReLU,
                  norm_layer=partial(nn.BatchNorm2d, eps=1e-6), ):
         super(FCUUp, self).__init__()
 
         self.up_stride = up_stride
         self.conv_project = nn.Conv2d(inplanes, outplanes, kernel_size=1, stride=1, padding=0)
+        self.linear_proj = proj
         self.bn = norm_layer(outplanes)
         self.act = act_layer()
 
-    def forward(self, x, H, W):
+    def forward(self, x, up_stride):
         B, _, C = x.shape
         # [N, 197, 384] -> [N, 196, 384] -> [N, 384, 196] -> [N, 384, 14, 14]
-        x_r = x[:, 1:].transpose(1, 2).reshape(B, C, H, W)
+        x = x.transpose(1, 2)
+        x_r = self.linear_proj(x).reshape(B, C, 8, 8)
         x_r = self.act(self.bn(self.conv_project(x_r)))
 
-        return F.interpolate(x_r, size=(H * self.up_stride, W * self.up_stride))
+        return F.interpolate(x_r, size=(8 * up_stride, 8 * up_stride))
 
 
 class Med_ConvBlock(nn.Module):
@@ -272,7 +275,7 @@ class ConvTransBlock(nn.Module):
     Basic module for ConvTransformer, keep feature maps for CNN block and patch embeddings for transformer encoder block
     """
 
-    def __init__(self, inplanes, outplanes, res_conv, stride, dw_stride, embed_dim, num_heads=12, mlp_ratio=4.,
+    def __init__(self, inplanes, outplanes, res_conv, stride, dw_stride, proj1, proj2, embed_dim, num_heads=12, mlp_ratio=4.,
                  qkv_bias=False, qk_scale=None, drop_rate=0., attn_drop_rate=0., drop_path_rate=0.,
                  last_fusion=False, num_med_block=0, groups=1):
 
@@ -293,9 +296,9 @@ class ConvTransBlock(nn.Module):
                 self.med_block.append(Med_ConvBlock(inplanes=outplanes, groups=groups))
             self.med_block = nn.ModuleList(self.med_block)
 
-        self.squeeze_block = FCUDown(inplanes=outplanes // expansion, outplanes=embed_dim, dw_stride=dw_stride)
+        self.squeeze_block = FCUDown(inplanes=outplanes // expansion, outplanes=embed_dim, dw_stride=dw_stride, proj=proj1)
 
-        self.expand_block = FCUUp(inplanes=embed_dim, outplanes=outplanes // expansion, up_stride=dw_stride)
+        self.expand_block = FCUUp(inplanes=embed_dim, outplanes=outplanes // expansion, up_stride=dw_stride, proj=proj2)
 
         self.trans_block = Block(
             dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
@@ -313,166 +316,17 @@ class ConvTransBlock(nn.Module):
 
         x_st = self.squeeze_block(x2, x_t)
 
-        print('x_st!!!!!!!!!!!!!!!!!!', x_st.shape)
-        print('x_t!!!!!!!!!!!!!!', x_t.shape)
-
+        # x_st: 6 4 512 ; x_t: 6 19 512
         x_t = self.trans_block(x_st + x_t)
 
         if self.num_med_block > 0:
             for m in self.med_block:
                 x = m(x)
 
-        x_t_r = self.expand_block(x_t, H // self.dw_stride, W // self.dw_stride)
+        x_t_r = self.expand_block(x_t, self.dw_stride)
         x = self.fusion_block(x, x_t_r, return_x_2=False)
 
         return x, x_t
-
-
-class Conformer(nn.Module):
-
-    def __init__(self, patch_size=16, in_chans=3, num_classes=1000, base_channel=64, channel_ratio=4, num_med_block=0,
-                 embed_dim=768, depth=12, num_heads=12, mlp_ratio=4., qkv_bias=False, qk_scale=None,
-                 drop_rate=0., attn_drop_rate=0., drop_path_rate=0.):
-
-        # Transformer
-        super().__init__()
-        self.num_classes = num_classes
-        self.num_features = self.embed_dim = embed_dim  # num_features for consistency with other models
-        assert depth % 3 == 0
-
-        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
-        self.trans_dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)]  # stochastic depth decay rule
-
-        # Classifier head
-        self.trans_norm = nn.LayerNorm(embed_dim)
-        self.trans_cls_head = nn.Linear(embed_dim, num_classes) if num_classes > 0 else nn.Identity()
-        self.pooling = nn.AdaptiveAvgPool2d(1)
-        self.conv_cls_head = nn.Linear(int(256 * channel_ratio), num_classes)
-
-        # Stem stage: get the feature maps by conv block (copied form resnet.py)
-        self.conv1 = nn.Conv2d(in_chans, 64, kernel_size=7, stride=2, padding=3, bias=False)  # 1 / 2 [112, 112]
-        self.bn1 = nn.BatchNorm2d(64)
-        self.act1 = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)  # 1 / 4 [56, 56]
-
-        # 1 stage
-        stage_1_channel = int(base_channel * channel_ratio)
-        trans_dw_stride = patch_size // 4
-        self.conv_1 = ConvBlock(inplanes=64, outplanes=stage_1_channel, res_conv=True, stride=1)
-        self.trans_patch_conv = nn.Conv2d(64, embed_dim, kernel_size=trans_dw_stride, stride=trans_dw_stride, padding=0)
-        self.trans_1 = Block(dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias,
-                             qk_scale=qk_scale, drop=drop_rate, attn_drop=attn_drop_rate, drop_path=self.trans_dpr[0],
-                             )
-
-        # 2~4 stage
-        init_stage = 2
-        fin_stage = depth // 3 + 1
-        for i in range(init_stage, fin_stage):
-            self.add_module('conv_trans_' + str(i),
-                            ConvTransBlock(
-                                stage_1_channel, stage_1_channel, False, 1, dw_stride=trans_dw_stride,
-                                embed_dim=embed_dim,
-                                num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
-                                drop_rate=drop_rate, attn_drop_rate=attn_drop_rate,
-                                drop_path_rate=self.trans_dpr[i - 1],
-                                num_med_block=num_med_block
-                            )
-                            )
-
-        stage_2_channel = int(base_channel * channel_ratio * 2)
-        # 5~8 stage
-        init_stage = fin_stage  # 5
-        fin_stage = fin_stage + depth // 3  # 9
-        for i in range(init_stage, fin_stage):
-            s = 2 if i == init_stage else 1
-            in_channel = stage_1_channel if i == init_stage else stage_2_channel
-            res_conv = True if i == init_stage else False
-            self.add_module('conv_trans_' + str(i),
-                            ConvTransBlock(
-                                in_channel, stage_2_channel, res_conv, s, dw_stride=trans_dw_stride // 2,
-                                embed_dim=embed_dim,
-                                num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
-                                drop_rate=drop_rate, attn_drop_rate=attn_drop_rate,
-                                drop_path_rate=self.trans_dpr[i - 1],
-                                num_med_block=num_med_block
-                            )
-                            )
-
-        stage_3_channel = int(base_channel * channel_ratio * 2 * 2)
-        # 9~12 stage
-        init_stage = fin_stage  # 9
-        fin_stage = fin_stage + depth // 3  # 13
-        for i in range(init_stage, fin_stage):
-            s = 2 if i == init_stage else 1
-            in_channel = stage_2_channel if i == init_stage else stage_3_channel
-            res_conv = True if i == init_stage else False
-            last_fusion = True if i == depth else False
-            self.add_module('conv_trans_' + str(i),
-                            ConvTransBlock(
-                                in_channel, stage_3_channel, res_conv, s, dw_stride=trans_dw_stride // 4,
-                                embed_dim=embed_dim,
-                                num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
-                                drop_rate=drop_rate, attn_drop_rate=attn_drop_rate,
-                                drop_path_rate=self.trans_dpr[i - 1],
-                                num_med_block=num_med_block, last_fusion=last_fusion
-                            )
-                            )
-        self.fin_stage = fin_stage
-
-        trunc_normal_(self.cls_token, std=.02)
-
-        self.apply(self._init_weights)
-
-    def _init_weights(self, m):
-        if isinstance(m, nn.Linear):
-            trunc_normal_(m.weight, std=.02)
-            if isinstance(m, nn.Linear) and m.bias is not None:
-                nn.init.constant_(m.bias, 0)
-        elif isinstance(m, nn.LayerNorm):
-            nn.init.constant_(m.bias, 0)
-            nn.init.constant_(m.weight, 1.0)
-        elif isinstance(m, nn.Conv2d):
-            nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-        elif isinstance(m, nn.BatchNorm2d):
-            nn.init.constant_(m.weight, 1.)
-            nn.init.constant_(m.bias, 0.)
-        elif isinstance(m, nn.GroupNorm):
-            nn.init.constant_(m.weight, 1.)
-            nn.init.constant_(m.bias, 0.)
-
-    @torch.jit.ignore
-    def no_weight_decay(self):
-        return {'cls_token'}
-
-    def forward(self, x):
-        B = x.shape[0]
-        cls_tokens = self.cls_token.expand(B, -1, -1)
-
-        # pdb.set_trace()
-        # stem stage [N, 3, 224, 224] -> [N, 64, 56, 56]
-        x_base = self.maxpool(self.act1(self.bn1(self.conv1(x))))
-
-        # 1 stage
-        x = self.conv_1(x_base, return_x_2=False)
-
-        x_t = self.trans_patch_conv(x_base).flatten(2).transpose(1, 2)
-        x_t = torch.cat([cls_tokens, x_t], dim=1)
-        x_t = self.trans_1(x_t)
-
-        # 2 ~ final
-        for i in range(2, self.fin_stage):
-            x, x_t = eval('self.conv_trans_' + str(i))(x, x_t)
-
-        # conv classification
-        x_p = self.pooling(x).flatten(1)
-        conv_cls = self.conv_cls_head(x_p)
-
-        # trans classification
-        x_t = self.trans_norm(x_t)
-        tran_cls = self.trans_cls_head(x_t[:, 0])
-
-        return [conv_cls, tran_cls]
-
 
 class NetG(nn.Module):
     def __init__(self, patch_size=16, in_chans=3, num_classes=1000, base_channel=64, channel_ratio=4, num_med_block=0,
@@ -505,13 +359,18 @@ class NetG(nn.Module):
                              qk_scale=qk_scale, drop=drop_rate, attn_drop=attn_drop_rate, drop_path=self.trans_dpr[0],
                              )
 
-        # 2~3 stage
+        self.linear1_proj = nn.Linear(64, 19)
+        self.linear2_proj = nn.Linear(19, 64)
+
+        # 2~3 stage 32x32
         init_stage = 2
         fin_stage = 4
         for i in range(init_stage, fin_stage):
             self.add_module('conv_trans_' + str(i),
                             ConvTransBlock(
-                                stage_1_channel, stage_1_channel, False, 1, dw_stride=trans_dw_stride,
+                                stage_1_channel, stage_1_channel, False, 1,
+                                dw_stride=4,
+                                proj1=self.linear1_proj, proj2=self.linear2_proj,
                                 embed_dim=embed_dim,
                                 num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
                                 drop_rate=drop_rate, attn_drop_rate=attn_drop_rate,
@@ -521,16 +380,18 @@ class NetG(nn.Module):
                             )
 
         stage_2_channel = 512
-        # 4~6 stage
+        # 4~6 stage; 4,5 64x64; 6 128x128
         init_stage = fin_stage  # 5
         fin_stage = 7  # 9
         for i in range(init_stage, fin_stage):
-            s = 1
+            stride = 16 if i == fin_stage-1 else 8
             in_channel = stage_1_channel if i == init_stage else stage_2_channel
             res_conv = True if i == init_stage else False
             self.add_module('conv_trans_' + str(i),
                             ConvTransBlock(
-                                in_channel, stage_2_channel, res_conv, s, dw_stride=trans_dw_stride // 2,
+                                in_channel, stage_2_channel, res_conv, 1,
+                                dw_stride=stride,
+                                proj1=self.linear1_proj, proj2=self.linear2_proj,
                                 embed_dim=embed_dim,
                                 num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
                                 drop_rate=drop_rate, attn_drop_rate=attn_drop_rate,
@@ -544,14 +405,15 @@ class NetG(nn.Module):
         init_stage = fin_stage  # 9
         fin_stage = 9  # 13
         for i in range(init_stage, fin_stage):
-            s = 1
             in_channel = stage_2_channel if i == init_stage else stage_3_channel
             out_channel = stage_3_channel if i == init_stage else stage_3_channel // 2
             res_conv = True if i == init_stage else False
             last_fusion = True if i == depth else False
             self.add_module('conv_trans_' + str(i),
                             ConvTransBlock(
-                                in_channel, out_channel, res_conv, s, dw_stride=trans_dw_stride // 4,
+                                in_channel, out_channel, res_conv, 1,
+                                dw_stride=32,
+                                proj1=self.linear1_proj, proj2=self.linear2_proj,
                                 embed_dim=embed_dim,
                                 num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
                                 drop_rate=drop_rate, attn_drop_rate=attn_drop_rate,
@@ -599,10 +461,9 @@ class NetG(nn.Module):
         x = x.view(x.size(0), 2 * self.ngf, 8, 8)
         x = self.conv1(x)
         x_t = self.linear1(c)
-
+        # interpolate in 1,2,4,6,8 stage
         x = F.interpolate(x, scale_factor=2)
         x = self.conv_1(x, return_x_2=False)
-
         x_t = self.trans_1(x_t)
 
         # 2 ~ final
